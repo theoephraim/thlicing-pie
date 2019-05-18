8990a23e04c8c23abb3e4028af13eac3846748c9
Aidan Musnitzky <musnit@gmail.com>
2019-05-18 13:31:27 -0400

    Add dev command

--- blockchain/contracts/PieOrg.sol
+++ blockchain/contracts/PieOrg.sol
@@ -1,17 +1,15 @@
 pragma solidity ^0.5.0;
 
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol';
-
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 /**
  * The PieOrg contract does it all
  */
 contract PieOrg is ERC20, ERC20Detailed {
-
     string public orgName;
 
-    constructor(string memory _orgName) ERC20Detailed(orgName, "SLICE", 18) public {
+    constructor(string memory _orgName) public ERC20Detailed(orgName, "SLICE", 18) {
         require(bytes(_orgName).length > 0);
         orgName = _orgName;
     }
